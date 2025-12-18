88ed62c369868e06ae34f4f8b3be8bae2c5c888a
Colin <141350351+colin-freed@users.noreply.github.com>
2025-12-18 15:58:48 -0500

    fix: favicon not showing in search results

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import "../app.css";
-  import favicon from "$lib/assets/favicon.svg";
   import { Header, Footer } from "$lib/components";
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="icon" sizes="any" href="/favicon.ico" />
 </svelte:head>
 
 <div class="min-h-screen bg-surface flex flex-col">
